ecdab4e6b16eee32b230bc4859f822e649585ca7
Dhamo1107 <dhamodharansathish4533@gmail.com>
2024-08-21 22:33:48 +0530

    Used before_create to set created status for task

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ApplicationRecord
   belongs_to :assignee, class_name: 'User', foreign_key: 'assigned_to_user_id'
 
   #=====ENUMS===========================================================================================================
-  enum status: { start: 0, in_progress: 1, completed: 2, cancelled: 3 }
+  enum status: { created: 0, viewed: 1, in_progress: 2, completed: 3, cancelled: 4 }
 
   #=====VALIDATIONS=====================================================================================================
   validates :title, presence: true, length: { maximum: 255 }
@@ -12,10 +12,17 @@ class Task < ApplicationRecord
   validates :status, inclusion: { in: statuses.keys }
   validate :assignee_is_not_creator
 
+  #=====CALLBACKS=======================================================================================================
+  before_create :set_default_status
+
   private
   def assignee_is_not_creator
     if assignee == creator
       errors.add(:assignee, "can't be the same as the creator")
     end
+  end
+
+  def set_default_status
+    self.status ||= :created
   end
 end
