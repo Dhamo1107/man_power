6e9da217a4c3827d7ca9e4d1ee50a1b5ca0bfc5d
Dhamo1107 <dhamodharansathish4533@gmail.com>
2024-09-06 20:10:46 +0530

    Added cname and aname methods in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def format_datetime(datetime)
     datetime.in_time_zone('Asia/Kolkata').strftime('%I:%M %p %d/%m/%Y')
   end
+
+  def cname?
+    controller.controller_name
+  end
+
+  def aname?
+    controller.action_name
+  end
 end
